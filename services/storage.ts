975da953164a7e0a64ddafd3172051c1632eb7da
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2024-11-01 23:05:12 +0530

    type export

--- services/storage.ts
+++ services/storage.ts
@@ -1,14 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const saveHeadlines = async (key: string, headlines: any[]) => {
+const saveHeadlines = async (key: string, headlines: any[]) => {
   await AsyncStorage.setItem(key, JSON.stringify(headlines));
 };
 
-export const getHeadlines = async (key: string) => {
+const getHeadlines = async (key: string) => {
   const data = await AsyncStorage.getItem(key);
   return data ? JSON.parse(data) : [];
 };
 
-export const clearStorage = async (key: string) => {
+const clearStorage = async (key: string) => {
   await AsyncStorage.removeItem(key);
 };
+
+export {saveHeadlines, getHeadlines, clearStorage};
