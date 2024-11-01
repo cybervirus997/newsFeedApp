3546221412652e723fa1781ecb3471f5b142e957
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2024-11-01 20:07:39 +0530

    base api sorted

---
+++ services/apiService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const endpointsHead = 'https://asia-south1-kc-stage-rp.cloudfunctions.net/';
+
+const fetchNews = async (query: string, from: string, to?: string) => {
+  const endpoint = `${endpointsHead}globalNews?endpoint=everything&q=${query}&from=${from}&sortBy=publishedAt${
+    to ? `&to=${to}` : ''
+  }`;
+  const response = await axios.post(endpoint, {
+    headers: {Accept: 'application/json'},
+  });
+  return response.data.articles;
+};
